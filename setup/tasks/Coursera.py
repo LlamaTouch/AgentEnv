7805aaff3e932c94b046c983ac95180afd0dfb15
xianqingjia777 <jxq@bupt.edu.cn>
2024-07-04 20:21:19 +0800

    fix the Coursera enroll logic with add robustness

--- setup/tasks/Coursera.py
+++ setup/tasks/Coursera.py
@@ -1,4 +1,4 @@
-from setup.tasks.BaseTaskSetup import BaseTaskSetup
+from setup.tasks.BaseTaskSetup import BaseTaskSetup,SetupFailureException
 from uiautomator2 import Device
 import time
 
@@ -12,53 +12,56 @@ def enroll_course(d: Device, course_name: str = "Algorithms, Part I"):
         # Click the "Search" button
         search_button = d(description="Search")
         if not search_button.exists(timeout=5):
-            raise Exception("Search button not found.")
+            raise SetupFailureException("Search button not found.")
         search_button.click()
 
         # Click and type in the search input field
         search_input = d(className="android.widget.EditText")
         if not search_input.exists(timeout=5):
-            raise Exception("Search input field not found.")
+            raise SetupFailureException("Search input field not found.")
         search_input.click()
         search_input.set_text(course_name)
         d.press("enter")
 
         # wait for search results to load
-        time.sleep(2)
+        time.sleep(5)
 
         # Click the first course in the search results
         first_course = d(className="android.view.View", instance=6)
         if not first_course.exists(timeout=5):
-            raise Exception("First course in search results not found.")
+            raise SetupFailureException("First course in search results not found.")
         first_course.click()
 
         # Navigate through the enrollment options
         see_options = d(text="See enrollment options")
         if not see_options.exists(timeout=5):
-            raise Exception("Enrollment options button not found.")
+            raise SetupFailureException("Enrollment options button not found.")
         see_options.click()
 
-        enroll_button = d(text=f"Enroll (Starts Jul 1)")
+        enroll_button = d(className="android.widget.Button", instance=0)
         if not enroll_button.exists(timeout=5):
-            raise Exception("Enroll button not found.")
+            raise SetupFailureException("Enroll button not found.")
         enroll_button.click()
-
-        # Check terms and conditions
-        terms_checkbox = d(className="android.widget.CheckBox", instance=3)
-        if not terms_checkbox.exists(timeout=5):
-            raise Exception("Terms and conditions checkbox not found.")
-        terms_checkbox.click()
-
-        # Commit to goals
-        commit_button = d(text="I Commit to My Goals")
-        if not commit_button.exists(timeout=5):
-            raise Exception("Commit button not found.")
-        commit_button.click()
         
-    except Exception as e:
-        print(f"An error occurred during the enrollment process: {e}")
-        # Additional error handling logic can be added here, such as logging the error or retrying the operation
+        # this process be required only for the first course enrollment
+        try:
+            # Check terms and conditions
+            terms_checkbox = d(className="android.widget.CheckBox", instance=3)
+            if terms_checkbox.exists(timeout=5):
+                terms_checkbox.click()
 
+            # Commit to goals
+            commit_button = d(text="I Commit to My Goals")
+            if commit_button.exists(timeout=5):
+                commit_button.click()
+
+        except Exception as e:
+            print(f"not first course enrollment: {e}")
+        
+    except SetupFailureException as e:
+        print(f"An error occurred during the enrollment process: {e}")
+        raise SetupFailureException("Unable to enroll in the course.")
+    
 def check_course_enrolled(d: Device, course_name: str ="Algorithms, Part I") -> bool:
     '''
     start from the default page of Coursera app: learn page
@@ -85,8 +88,8 @@ class CourseraTask01(BaseTaskSetup):
         time.sleep(2)
 
         # make enrollment to the course 'Algorithms, Part I'
-        if not check_course_enrolled(self.d):
-            enroll_course(self.d)
+        if not check_course_enrolled(self.d,"Algorithms, Part I"):
+            enroll_course(self.d,"Algorithms, Part I")
             
         # stop app
         self.d.press("home")
@@ -107,8 +110,8 @@ class CourseraTask02(BaseTaskSetup):
         time.sleep(2)
 
         # make enrollment to the course 'Algorithms, Part I'
-        if not check_course_enrolled(self.d):
-            enroll_course(self.d)
+        if not check_course_enrolled(self.d,"Algorithms, Part I"):
+            enroll_course(self.d,"Algorithms, Part I")
             
         # stop app
         self.d.press("home")
