056cde6a66cc55f4048aa4db348cad1af3db8137
xianqingjia777 <jxq@bupt.edu.cn>
2024-07-08 20:57:12 +0800

    fix task03

--- setup/tasks/Quora.py
+++ setup/tasks/Quora.py
@@ -136,52 +136,14 @@ class QuoraTask03(BaseTaskSetup):
     def setup(self):
         # Start the Quora app
         self.d.app_start("com.quora.android")
+        time.sleep(5)
+
+        # bookmark a post
+        bookmark_post(self.d)
+    
+        self.d.press("home")
         time.sleep(2)
-        
-        # Navigate to the following page
-        try:
-            # Access the badge wrapper to navigate to the intended section
-            navigation_path = '//android.widget.RelativeLayout[@resource-id="com.quora.android:id/badge_wrapper"])[2]'
-            self.d.xpath(navigation_path).click()
-            time.sleep(2)
-            
-            # Wait for the "Spaces you might like" text view to appear
-            text_view = self.d.xpath('//android.widget.TextView[@text="Spaces you might like"]')
-            if text_view.wait():
-                # Navigate to the first recommended space
-                view = text_view.child('.//android.view.View[1]')
-                if view.exists():
-                    view.click()
-                    time.sleep(5)
-                    
-                    # Attempt to go to the first post
-                    post = self.d.xpath('//android.webkit.WebView[@text="Quora"]/android.view.View/android.view.View[5]')
-                    if post.wait():
-                        post.click()
-                        time.sleep(5)
-                        
-                        # Attempt to click the 'More' button and bookmark the post
-                        if self.d(text="More").exists():
-                            self.d(text="More").click()
-                            time.sleep(2)
-                            if self.d(text="Bookmark").exists():
-                                self.d(text="Bookmark").click()
-                            else:
-                                print("Bookmark option not found.")
-                        else:
-                            print("More button not found.")
-                    else:
-                        print("First post not found.")
-                else:
-                    print("Specified view does not exist.")
-            else:
-                print("Specified TextView 'Spaces you might like' not found.")
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-        finally:
-            # Ensure the app is stopped even if an error occurs
-            self.d.press("home")
-            time.sleep(2)
-            self.d.app_stop("com.quora.android")
+        self.d.app_stop("com.quora.android")
+
 
 
